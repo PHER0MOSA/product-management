945e21b443f3bfa002a79d15f002ccd8b56910af
PHER0MOSA <naoki.nakamura.0621@gmail.com>
2026-07-18 23:31:15 +0900

    わざとテストで失敗する

--- server/internal/product/domain/product_price.go
+++ server/internal/product/domain/product_price.go
@@ -7,7 +7,7 @@ type ProductPrice struct {
 
 // NewProductPrice は商品価格を生成します。
 func NewProductPrice(v int64) (ProductPrice, error) {
-	if v < 0 {
+	if v < 1000 {
 		return ProductPrice{}, ErrInvalidProductPrice
 	}
 	return ProductPrice{value: v}, nil
