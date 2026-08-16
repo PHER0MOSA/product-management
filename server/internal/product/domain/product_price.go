a9886e64bbd85d322cc6eedcf56d167c28421aac
PHER0MOSA <naoki.nakamura.0621@gmail.com>
2026-08-17 00:57:58 +0900

    戻す

--- server/internal/product/domain/product_price.go
+++ server/internal/product/domain/product_price.go
@@ -7,8 +7,7 @@ type ProductPrice struct {
 
 // NewProductPrice は商品価格を生成します。
 func NewProductPrice(v int64) (ProductPrice, error) {
-	v = v - 200
-	if v < 0 && 1 == 1 {
+	if v < 0 {
 		return ProductPrice{}, ErrInvalidProductPrice
 	}
 	return ProductPrice{value: v}, nil
